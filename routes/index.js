3e215f1b03a34e874e6fa203ad69422c252f7a6f
robyafrizal <robyafrizal86@gmail.com>
2024-08-05 22:15:54 +0700

    create user endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,23 @@
-//import express
-const express = require("express");
-
-//init express router
-const router = express.Router();
+const express = require("express"); //import express
+const router = express.Router(); //init express router
+const verifyToken = require("../middlewares/auth"); //import verifyToken
 
 //import register controller
 const registerController = require("../controllers/RegisterController");
+//import login controller
+const loginController = require("../controllers/LoginController");
+//import user controller
+const userController = require("../controllers/UserController");
 
 //import validate register
-const { validateRegister } = require("../utils/validators/auth");
+const { validateRegister, validateLogin } = require("../utils/validators/auth");
+const { validateUser } = require("../utils/validators/user");
 
-//define route for register
+//define route for register & login
 router.post("/register", validateRegister, registerController.register);
+router.post("/login", validateLogin, loginController.login);
+router.get("/users", verifyToken, userController.findUsers);
+router.post("/users", verifyToken, validateUser, userController.createUser);
 
 //export router
 module.exports = router;
