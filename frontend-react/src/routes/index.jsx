562a0edde0d56272a7c76c8ffa325806ea755b35
robyafrizal <robyafrizal86@gmail.com>
2024-08-19 22:28:12 +0700

    project completed temporarily

--- frontend-react/src/routes/index.jsx
+++ frontend-react/src/routes/index.jsx
@@ -1,8 +1,8 @@
 //import useContext
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
 //import context
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from "../context/AuthContext";
 
 //import react router dom
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -16,25 +16,74 @@ import Register from "../views/auth/register.jsx";
 //import view login
 import Login from "../views/auth/login.jsx";
 
+//import view dashboard
+import Dashboard from "../views/dashboard/index.jsx";
+
+//import view users index
+import UsersIndex from "../views/users/index.jsx";
+
+//import view users create
+import UsersCreate from "../views/users/create.jsx";
+
+//import view users edit
+import UsersEdit from "../views/users/edit.jsx";
+
 export default function AppRoutes() {
+  //destructure context "isAuthenticated"
+  const { isAuthenticated } = useContext(AuthContext);
 
-    //destructure context "isAuthenticated"
-    const { isAuthenticated } = useContext(AuthContext);
+  return (
+    <Routes>
+      {/* route "/" */}
+      <Route path="/" element={<Home />} />
 
-    return (
-        <Routes>
-            {/* route "/" */}
-            <Route path="/" element={<Home />} />
+      {/* route "/register" */}
+      <Route
+        path="/register"
+        element={
+          isAuthenticated ? <Navigate to="/dashboard" replace /> : <Register />
+        }
+      />
 
-            {/* route "/register" */}
-            <Route path="/register" element={
-                isAuthenticated ? <Navigate to="/dashboard" replace /> : <Register />
-            } />
+      {/* route "/login" */}
+      <Route
+        path="/login"
+        element={
+          isAuthenticated ? <Navigate to="/dashboard" replace /> : <Login />
+        }
+      />
 
-            {/* route "/login" */}
-            <Route path="/login" element={
-                isAuthenticated ? <Navigate to="/dashboard" replace /> : <Login />
-            } />
-        </Routes>
-    );
+      {/* route "/dashboard" */}
+      <Route
+        path="/dashboard"
+        element={
+          isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />
+        }
+      />
+
+      {/* route "/users" */}
+      <Route
+        path="/users"
+        element={
+          isAuthenticated ? <UsersIndex /> : <Navigate to="/login" replace />
+        }
+      />
+
+      {/* route "/users/create" */}
+      <Route
+        path="/users/create"
+        element={
+          isAuthenticated ? <UsersCreate /> : <Navigate to="/login" replace />
+        }
+      />
+
+      {/* route "/users/edit/:id" */}
+      <Route
+        path="/users/edit/:id"
+        element={
+          isAuthenticated ? <UsersEdit /> : <Navigate to="/login" replace />
+        }
+      />
+    </Routes>
+  );
 }
